2e49aa313020ea0015050e6efd0f8171869c48f8
Sanghun1Adam1Park <sanghunpark0628@gamil.com>
2025-05-23 14:57:17 +0200

    Webclient.py modificaiton done

--- webclient.py
+++ webclient.py
@@ -4,7 +4,7 @@ import sys
 
 ENCODING: str = "ISO-8859-1"
 
-def client(address: str, port: int) -> str:
+def get(address: str, port: int) -> str:
     ip: str = socket.gethostbyname(address)
     ip_addr: Tuple[str, int] = (ip, port)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
@@ -31,6 +31,23 @@ def client(address: str, port: int) -> str:
         
     return response.decode(ENCODING)
 
+def post(address: str, port: int, payload: str, content_type: str="text/pain") -> None:
+    ip: str = socket.gethostbyname(address)
+    ip_addr: Tuple[str, int] = (ip, port)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
+        s.connect(ip_addr)
+        request: str = (
+            "POST / HTTP/1.1\r\n"
+            f"Host: {address}\r\n"
+            "Connection: close\r\n"
+            f"Content-Type: {content_type}\r\n"
+            f"Content-Length: {len(payload.encode(ENCODING))}\r\n"
+            "\r\n"
+            f"{payload}\r\n"
+            "\r\n"
+        )
+        s.sendall(request.encode(ENCODING))
+
 if __name__ == "__main__":
     if len(sys.argv) == 2 or len(sys.argv) == 3:
         address: str = sys.argv[1]
@@ -38,8 +55,9 @@ if __name__ == "__main__":
             port: int = 80
         else:
             port: int = int(sys.argv[2]) 
-        response = client(address, port)
+        response = get(address, port)
         print(response)
+        post(address, port, "Hello, Server")
     else: 
         print("Usage: python webclient.py <address> <port>[optional]")
         sys.exit(1)
