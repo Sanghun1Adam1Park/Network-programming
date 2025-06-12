137af1d08bd843b8fd0e002b23c1e30f90dbf7ca
Sanghun1Adam1Park <sanghunpark0628@gmail.com>
2025-06-12 12:20:23 +0200

    AT-1: planning

---
+++ atomic_time/client.py
@@ -0,0 +1,9 @@
+import socket
+
+if __name__ == "__main__":
+  # Connect to the server time.nist.gov on port 37
+  # Receive data. (You don’t need to send anything.) You should get 4 bytes.
+  # The 4 bytes represent a 4-byte big-endian number. Decode the 4 bytes with .from_bytes() into a numeric variable.
+  # Print out the value from the time server, which should be the number of seconds since January 1, 1900 00:00.
+  # Print the system time as number of seconds since January 1, 1900 00:00.
+  pass
